98b532012f23a9d76e2028153923de38d5e99802
aravindpolavarapu96-oss <aravindpolavarapu96@gmail.com>
2026-06-21 14:34:55 +0530

    New project structure defined

---
+++ src/main/java/com/employe/aravind/AravindApplication.java
@@ -0,0 +1,13 @@
+package com.employe.aravind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AravindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AravindApplication.class, args);
+	}
+
+}
